904d4c30a5e99cc785d817f100397e013692b67f
yuxingao2 <yuxingao2@creditease.cn>
2021-03-09 15:14:22 +0800

    Quick Start

--- spark-tutorial-2.4.7/quick-start/src/main/scala/SimpleApp.scala
+++ spark-tutorial-2.4.7/quick-start/src/main/scala/SimpleApp.scala
@@ -1,8 +1,9 @@
+
 import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
+    val logFile = "/apps/pkgs/spark-2.4.7-bin-hadoop2.7/README.md" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
