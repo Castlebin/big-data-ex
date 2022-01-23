688dd3c6f72f59c65f94c915c1a077cea2c09b38
Castlein <Castlebin@gmail.com>
2022-01-23 15:01:57 +0800

    Flink Basic Demos

---
+++ flink-basic-demo/README.md
@@ -0,0 +1,7 @@
+# Flink 的基本使用
+
+## Flink 程序的基本结构
+- Source
+- Transformation
+- Sink
+
---
+++ flink-basic-demo/src/main/java/flink/learn/Demo1.java
@@ -0,0 +1,9 @@
+package flink.learn;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
