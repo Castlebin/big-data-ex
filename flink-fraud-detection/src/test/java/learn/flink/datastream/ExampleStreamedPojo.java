05aa401f0732cf30c943bb3dc16190f332bcaaff
Castlein <Castlebin@gmail.com>
2022-01-23 14:12:57 +0800

    What can be Streamed?

--- flink-fraud-detection/src/test/java/learn/flink/datastream/ExampleStreamedPojo.java
+++ flink-fraud-detection/src/test/java/learn/flink/datastream/ExampleStreamedPojo.java
@@ -1,12 +1,18 @@
 package learn.flink.datastream;
 
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.junit.jupiter.api.Test;
 
 public class ExampleStreamedPojo {
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * 测试 Flink 可以对 普通的 POJO 进行序列化
+     * （注意 Person 并没有实现 Serializable 接口 ）
+     */
+    @Test
+    public void testSerialize() throws Exception {
         final StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.getExecutionEnvironment();
 
