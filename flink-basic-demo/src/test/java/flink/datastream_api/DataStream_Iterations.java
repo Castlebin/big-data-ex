ae5e93fb9faddce2d941ea91e4aff68b8c7c34a7
Castlein <Castlebin@gmail.com>
2022-01-29 17:34:37 +0800

    IterativeStream

--- flink-basic-demo/src/test/java/flink/datastream_api/DataStream_Iterations.java
+++ flink-basic-demo/src/test/java/flink/datastream_api/DataStream_Iterations.java
@@ -2,29 +2,57 @@ package flink.datastream_api;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.IterativeStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class DataStream_Iterations {
-    public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+    private static StreamExecutionEnvironment env;
+
+    @BeforeAll
+    public static void before() {
+        env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+
+    /**
+     * IterativeStream
+     */
+    @Test
+    public void iterative_example_1() throws Exception {
         // 生成数字序列
-        DataStreamSource<Long> someIntegers = env.fromSequence(0, 5);
+        DataStream<Long> someIntegers = env.fromSequence(0, 20);
+
         IterativeStream<Long> iteration = someIntegers.iterate();
 
-        SingleOutputStreamOperator<Long> minusOne = iteration.map((MapFunction<Long, Long>) value -> value - 1);
-        SingleOutputStreamOperator<Long> stillGreaterThanZero = minusOne.filter((FilterFunction<Long>) value -> value > 0);
+        DataStream<Long> minusOne = iteration.map(new MapFunction<Long, Long>() {
+            @Override
+            public Long map(Long value) throws Exception {
+                return value - 1 ;
+            }
+        });
 
-        // closeWith 操作，会处理数据流，并将处理结果反馈给原始流的头部
+        DataStream<Long> stillGreaterThanZero = minusOne.filter(new FilterFunction<Long>() {
+            @Override
+            public boolean filter(Long value) throws Exception {
+                return (value > 0);
+            }
+        });
+
         iteration.closeWith(stillGreaterThanZero);
 
-        SingleOutputStreamOperator<Long> lessOrEqualsZero = minusOne.filter((FilterFunction<Long>) value -> value <= 0);
+        DataStream<Long> lessThanZero = minusOne.filter(new FilterFunction<Long>() {
+            @Override
+            public boolean filter(Long value) throws Exception {
+                return (value <= 0);
+            }
+        });
 
-        lessOrEqualsZero.print();
+        lessThanZero.print();
 
         env.execute();
     }
+
 }
