1df061f2c227085ee28e63d54800f837eba0004b
Castlein <Castlebin@gmail.com>
2022-02-13 00:36:35 +0800

    一个更复杂点版的 flatMap , RichFlatMapFunction ，新增的  1.14.0 版本里还没有  定义了一个去重的方法，利用了 flink 的状态管理

--- flink-basic-demo/src/test/java/flink/operator/Deduplicator.java
+++ flink-basic-demo/src/test/java/flink/operator/Deduplicator.java
@@ -11,6 +11,13 @@ import org.apache.flink.util.Collector;
  * 一个更复杂点版的 flatMap , RichFlatMapFunction ，新增的  1.14.0 版本里还没有 (看原理其实可以自己在现有的API上实现)
  *
  * 定义了一个去重的方法，利用了 flink 的状态管理
+ *
+ * 清理状态 #
+ * 上面例子有一个潜在的问题：当键空间是无界的时候将发生什么？Flink 会对每个使用过的键都存储一个 Boolean
+ * 类型的实例。如果是键是有限的集合还好，但在键无限增长的应用中，清除再也不会使用的状态是很必要的。这通过在状态对象上调用 clear() 来实现，如下：
+ *
+ * keyHasBeenSeen.clear()
+ * 对一个给定的键值，你也许想在它一段时间不使用后来做这件事。
  */
 public class Deduplicator<Event> extends RichFlatMapFunction<Event, Event> {
     ValueState<Boolean> keyHasBeenSeen;
