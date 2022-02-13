5daef0ba3093254bd8001bfbb2cbc35e55456767
Castlein <Castlebin@gmail.com>
2022-02-14 05:21:32 +0800

    connect 操作 ，将两个流放到一起，返回一个 ConnectedStream，但两个流其实依然独立， 如果后续还要对 ConnectedStream 进行 map、flatMap、filter 等操作，必须传入两个对应的处理方法，分别对连接前的流1、流2 进行处理 也就是说，连接后的流，内部其实依然保持独立！

--- flink-basic-demo/src/test/java/flink/transformation/TransformationTest01.java
+++ flink-basic-demo/src/test/java/flink/transformation/TransformationTest01.java
@@ -1,8 +1,10 @@
 package flink.transformation;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 import org.apache.flink.util.Collector;
 import org.junit.jupiter.api.Test;
 
@@ -66,6 +68,42 @@ public class TransformationTest01 {
                 .print();
 
         env.execute("filter");
+    }
+
+    /**
+     * 4. connect 操作 ，将两个流放到一起，返回一个 ConnectedStream，但两个流其实依然独立，
+     * 如果后续还要对 ConnectedStream 进行 map、flatMap、filter 等操作，必须传入两个对应的处理方法，分别对连接前的流1、流2 进行处理
+     * 也就是说，连接后的流，内部其实依然保持独立！
+     */
+    @Test
+    public void connect() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<Long> stream01 = env.fromSequence(1, 10);
+        DataStreamSource<String> stream02 = env.readTextFile("data/hello-world.txt");
+
+        ConnectedStreams<Long, String> connected = stream01
+                .connect(stream02);
+
+        connected.map(new CoMapFunction<Long, String, Object>() {
+
+            // 这里实现对连接前的 流1 的操作
+            @Override
+            public Object map1(Long aLong) throws Exception {
+                return aLong * 2;
+            }
+
+            // 这里实现对连接前的 流2 的操作
+            @Override
+            public Object map2(String s) throws Exception {
+                return ":2:" + s;
+            }
+        })
+                .print()
+        ;
+
+
+        env.execute("connect");
     }
 
 }
