324aad7bb95d335aa83d83228ccdd50e00ad0020
Castlein <Castlebin@gmail.com>
2022-02-27 01:17:27 +0800

    测试数据 及 pojo

---
+++ flink-basic-demo/src/test/java/flink/pojo/UserReport.java
@@ -0,0 +1,79 @@
+package flink.pojo;
+
+import java.io.Serializable;
+
+public class UserReport implements Serializable {
+
+    private long creativeId;
+    private long unitId;
+    private long accountId;
+    private long photoId;
+    private long userId;
+    private long createTime;
+    private long adminId;
+    private long adminTime;
+
+    public long getCreativeId() {
+        return creativeId;
+    }
+
+    public void setCreativeId(long creativeId) {
+        this.creativeId = creativeId;
+    }
+
+    public long getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(long unitId) {
+        this.unitId = unitId;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public long getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(long photoId) {
+        this.photoId = photoId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
+
+    public long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(long adminId) {
+        this.adminId = adminId;
+    }
+
+    public long getAdminTime() {
+        return adminTime;
+    }
+
+    public void setAdminTime(long adminTime) {
+        this.adminTime = adminTime;
+    }
+}
